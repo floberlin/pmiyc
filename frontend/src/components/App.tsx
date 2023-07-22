bed3e2736fc6ad1076f427599e9d1881c5f2d4f8
normadn <norman.saade@gmail.com>
2023-07-22 12:25:43 +0200

    smart account addr pased to sismo

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,4 +1,10 @@
 import styles from "../styles/Home.module.css";
+import {
+  SismoConnectButton,
+  AuthType,
+  SismoConnectResponse,
+  ClaimType,
+} from "@sismo-core/sismo-connect-react";
 import { useState, useEffect, useRef } from "react";
 import SocialLogin from "@biconomy/web3-auth";
 import { ChainId } from "@biconomy/core-types";
@@ -18,6 +24,8 @@ export default function App() {
   const [provider, setProvider] = useState<any>(null);
   const [userInfo, setUserInfo] = useState<any>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  console.log("smartAccount", smartAccount);
 
   function truncateAddress(add: string) {
     const len = add.length;
@@ -131,6 +139,56 @@ export default function App() {
           alt="Picture of the author"
           className={styles.imageItem}
         />
+        {!!smartAccount && (
+          <SismoConnectButton
+            config={{
+              appId: "0x45792cd187672019da6ee08aef36eb46", // replace with your appId
+              vault: {
+                // For development purposes insert the Data Sources that you want to impersonate here
+                // Never use this in production
+                impersonate: [
+                  // EVM
+                  "leo21.sismo.eth",
+                  "0xA4C94A6091545e40fc9c3E0982AEc8942E282F38",
+                  "0x1b9424ed517f7700e7368e34a9743295a225d889",
+                  "0x82fbed074f62386ed43bb816f748e8817bf46ff7",
+                  "0xc281bd4db5bf94f02a8525dca954db3895685700",
+                  // Github
+                  "github:leo21",
+                  // Twitter
+                  "twitter:leo21_eth",
+                  // Telegram
+                  "telegram:leo21",
+                ],
+              },
+              // displayRawResponse: true,
+            }}
+            // request proof of Data Sources ownership (e.g EVM, GitHub, twitter or telegram)
+            auths={[{ authType: AuthType.VAULT }]}
+            // request zk proof that Data Source are part of a group
+            // (e.g NFT ownership, Dao Participation, GitHub commits)
+            claims={[
+              // ENS DAO Voters
+              { groupId: "0x85c7ee90829de70d0d51f52336ea4722" },
+            ]}
+            // request message signature from users.
+            signature={{
+              message: (smartAccount as SmartAccount).address.toString(),
+            }}
+            // retrieve the Sismo Connect Reponse from the user's Sismo data vault
+            onResponse={async (response: SismoConnectResponse) => {
+              const res = await fetch("/api/verify", {
+                method: "POST",
+                body: JSON.stringify(response),
+              });
+              console.log(await res.json());
+            }}
+            // reponse in bytes to call a contract
+            // onResponseBytes={async (response: string) => {
+            //   console.log(response);
+            // }}
+          />
+        )}
         <div className={styles.header}>
           <h1 className="text-3xl text-white font-bold underline">
             Connect and start staking
