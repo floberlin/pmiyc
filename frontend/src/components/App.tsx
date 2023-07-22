5edd5386fccbb68bd9c2fe1fe4e816b98b475b1a
Tobias Leinß <tobias.leinss@mway.io>
2023-07-22 15:35:53 +0200

    Fix biconomy frontend

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -9,15 +9,20 @@ import { useState, useEffect, useRef } from "react";
 import SocialLogin from "@biconomy/web3-auth";
 import { ChainId } from "@biconomy/core-types";
 import { ethers } from "ethers";
-import SmartAccount from "@biconomy/smart-account";
 import Swapper from "./Swapper";
 import Spinner from "./Spinner";
 import Onboarder from "./Onboarder";
 import Image from "next/image";
 import Menu from "./Menu";
+import {
+  BiconomySmartAccount,
+  BiconomySmartAccountConfig,
+} from "@biconomy/account";
 
 export default function App() {
-  const [smartAccount, setSmartAccount] = useState<SmartAccount | null>(null);
+  const [smartAccount, setSmartAccount] = useState<BiconomySmartAccount | null>(
+    null
+  );
   const [interval, enableInterval] = useState(false);
   const sdkRef = useRef<SocialLogin | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
@@ -49,13 +54,13 @@ export default function App() {
     if (!sdkRef.current) {
       const socialLoginSDK = new SocialLogin();
       const signature1 = await socialLoginSDK.whitelistUrl(
-        "https://aaswap.vercel.app"
+        "http://127.0.0.1:5173/"
       );
       await socialLoginSDK.init({
-        chainId: ethers.utils.hexValue(ChainId.POLYGON_MAINNET).toString(),
-        network: "mainnet",
+        chainId: ethers.utils.hexValue(ChainId.POLYGON_MUMBAI).toString(),
+        network: "testnet",
         whitelistUrls: {
-          "https://aaswap.vercel.app": signature1,
+          "http://127.0.0.1:3000/": signature1,
         },
       });
       sdkRef.current = socialLoginSDK;
@@ -69,41 +74,38 @@ export default function App() {
   }
 
   async function setupSmartAccount() {
-    if (!sdkRef?.current?.provider) {
-      setUserInfo(await sdkRef?.current?.getUserInfo());
-      console.log(userInfo);
-      return;
-    }
+    if (!sdkRef?.current?.provider) return;
     sdkRef.current.hideWallet();
     setLoading(true);
     const web3Provider = new ethers.providers.Web3Provider(
       sdkRef.current.provider
     );
     setProvider(web3Provider);
+
     try {
-      const smartAccount = new SmartAccount(web3Provider, {
-        activeNetworkId: ChainId.POLYGON_MAINNET,
-        supportedNetworksIds: [ChainId.POLYGON_MAINNET],
-        networkConfig: [
-          {
-            chainId: ChainId.POLYGON_MAINNET,
-            dappAPIKey: process.env.NEXT_PUBLIC_BICONOMY_API_KEY,
-          },
-        ],
-      });
-      const acct = await smartAccount.init();
-      console.log({
-        deployed: await smartAccount.isDeployed(ChainId.POLYGON_MAINNET),
-      });
-      const isDeployed = await smartAccount.isDeployed(ChainId.POLYGON_MAINNET);
-      if (isDeployed == false) {
-        console.log("this one needs to be deployed");
-        const deployTx = await smartAccount.deployWalletUsingPaymaster();
-        console.log(deployTx);
-      }
-      setSmartAccount(acct);
-      const info = await sdkRef.current.getUserInfo();
-      setUserInfo(info);
+      const biconomySmartAccountConfig: BiconomySmartAccountConfig = {
+        signer: web3Provider.getSigner(),
+        chainId: ChainId.POLYGON_MUMBAI,
+        // bundler: bundler,
+        // paymaster: paymaster,
+      };
+      let biconomySmartAccount = new BiconomySmartAccount(
+        biconomySmartAccountConfig
+      );
+      biconomySmartAccount = await biconomySmartAccount.init();
+      console.log("owner: ", biconomySmartAccount.owner);
+      console.log(
+        "address: ",
+        await biconomySmartAccount.getSmartAccountAddress()
+      );
+      console.log(
+        "deployed: ",
+        await biconomySmartAccount.isAccountDeployed(
+          await biconomySmartAccount.getSmartAccountAddress()
+        )
+      );
+
+      setSmartAccount(biconomySmartAccount);
       setLoading(false);
     } catch (err) {
       console.log("error setting up smart account... ", err);
