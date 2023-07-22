faae5ac1439b3a88b9485d1dafbda239a789ab09
normadn <norman.saade@gmail.com>
2023-07-22 21:19:26 +0200

    new ui for factory

---
+++ factory/pages/_app.tsx
@@ -0,0 +1,38 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import type { AppProps } from "next/app";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { goerli } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { alchemyProvider } from "@wagmi/core/providers/alchemy";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [goerli],
+  [alchemyProvider({ apiKey: "8kMhSrpLGyIlRYBtAtT9IAVWeVK8hiOZ" })]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "RainbowKit App",
+  projectId: "zkPaymasterFactory",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
